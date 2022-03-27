618f2f77cd304de610c5c05c21657f577c24a296
Kylie Fauquher <44852559+kfauquher@users.noreply.github.com>
2022-03-27 14:40:02 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -19,11 +19,13 @@ words = pickle.load(open("words.pkl", "rb"))
 classes = pickle.load(open("classes.pkl", "rb"))
 model = load_model("chatbot_AI.h5")
 
+#We are organizing our text again by returning everything to the base word.
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [wordlemm.lemmatize(word) for word in sentence_words]
     return sentence_words
 
+#The machine learning is extracting words from the text
 def bag_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
@@ -33,18 +35,21 @@ def bag_words(sentence):
                 bag[i] = 1
     return np.array(bag)
 
+#This will look for and use the best possible option
 def predict_class(sentence):
     bow = bag_words(sentence)
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
     result = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
+    
+#A lambda expression can implement a functional interface by defining an anonymous function that can be passed as an argument to some method. It reduces amount of code.
     result.sort(key = lambda x: x[1], reverse = True)
-    return_list = []
+    prob_list = []
     for r in result:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
+        prob_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return prob_list
 
+#Returns our response
 def get_response(intents_list,intents_json):
     tag = intents_list[0]["intent"]
     list_of_intents = intents_json["intent"]
